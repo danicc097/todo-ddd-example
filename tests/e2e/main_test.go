74bff1ce5f3aabf6a3a4543859a37a59b01b6750
Daniel Castro <danicc097@gmail.com>
2026-02-17 18:34:30 +0100

    e2e goleak fix idle conns

--- tests/e2e/main_test.go
+++ tests/e2e/main_test.go
@@ -3,11 +3,24 @@
 package e2e
 
 import (
+	"net/http"
+	"os"
 	"testing"
 
 	"go.uber.org/goleak"
 )
 
 func TestMain(m *testing.M) {
-	goleak.VerifyTestMain(m)
+	code := m.Run()
+
+	if tr, ok := http.DefaultTransport.(*http.Transport); ok {
+		tr.CloseIdleConnections()
+	}
+
+	if err := goleak.Find(); err != nil {
+		os.Stderr.WriteString("goleak: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	os.Exit(code)
 }
